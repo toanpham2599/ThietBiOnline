b3eb3a636e3a13ec89563c43461b8f2851ca076b
= <=>
2020-06-23 09:14:03 +0700

    update bracnh

--- UnitTest/Properties/AssemblyInfo.cs
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("71848403-7072-411d-be82-70d3f2f9b3e0")]
+//[assembly: Guid("71848403-7072-411d-be82-70d3f2f9b3e0")]
 
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
